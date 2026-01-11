83062b5baa049bb07f52140831aff115784b6b54
sonnelon <sonnelon4@gmail.com>
2026-01-11 15:13:43 +0300

    update: readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Uartmon
 ```This is a simple UART monitor written as a pragmatic utility, not a pythonic showcase.```
+
+1. Clone repo: `git clone https://github.com/sonnelon/uartmon`
+2. Install dependencies: `./install.sh`
+3. Activate virtual environment: `source .venv/bin/activate`
+4. Run: `python uartmon --help`
--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "The installation has started."
 python -m venv .venv
+echo "[1] Virtual environment has been initialized."
 source .venv/bin/activate
+echo "[2] Virtual environment has been activated."
 pip install -r dependencies.dat
+echo "[3] Installation of dependencies has been completed"
 
 echo "Setup complete. Activate with: source .venv/bin/activate"
