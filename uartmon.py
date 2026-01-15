2426bb90e0c1a42e1c035c937fda61bdde0ede06
sonnelon <sonnelon4@gmail.com>
2026-01-15 14:20:45 +0300

    update: starting refactoring

--- uartmon.py
+++ uartmon.py
@@ -1,81 +1,62 @@
-import serial, click, os, time, sys
-from threading import Thread
+import serial, click, time, logging
+from threading import Thread, Event
+from os import system
 
-running = True
-output_file = ""
+READ_SIZE = 1024
 
-def log_info(msg):
-	ts = time.time()
-	info_msg = f"[info] [{ts:.6f}] {msg}"
-	if output_file:
-		print(info_msg, file=output_file)
-		return
-	print(info_msg)
+def setup_logger(output: str | None):
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] [%(asctime)s] %(message)s", datefmt="%H:%M:%S", filename=output if output else None)
 
-def log_err(msg): 
-	ts = time.time()
-	err_msg = f"[error] [{ts:.6f}] {msg}"
-	if output_file:
-		print(err_msg, file=output_file)
-		return
-	print(err_msg)
+class Uartmon:
+    def __init__(self, port: str, baud: int, output: str | None):
+        self.serial = serial.Serial(port, baudrate=baud, timeout=0.1)
+        self.stop_event = Event()
+        setup_logger(output)
 
-def read_data(port): 
-	global running
-	size_read_data = 1024
+    def start(self, not_clear):
+        if not not_clear: system("clear")
+        self.rx_thread = Thread(target=self._read_loop)
+        self.tx_thread = Thread(target=self._write_loop)
+        self.rx_thread.start()
+        self.tx_thread.start()
 
-	while running:
-		data = port.read(size=size_read_data)
-		if not data: continue
-		log_info(f"RX {data.hex()}")
+    def stop(self):
+        self.stop_event.set()
+        self.serial.close()
 
-def write_data(port):
-	global running
+    def _write_loop(self):
+        while not self.stop_event.is_set():
+            try:
+                data = input("uartmon > ")
+                self.serial.write(data.encode())
+            except EOFError: break
 
-	while running:
-		data = input("uartmon > ")
-		port.write(data.encode())
+    def _read_loop(self):
+        while not self.stop_event.is_set():
+            data = self.serial.read(READ_SIZE)
+            if not data: continue
 
-def create_serial(port, baud): return serial.Serial(port=port, baudrate=baud, timeout=0.1)
+            decoded = data.decode()
+            logging.info("[RX] hex=%s | text=%s", data.hex(), decoded.strip())
 
 @click.command()
-@click.option("--port", default="/dev/ttyUSB0", help="Your serial port.")
-@click.option("--read", "only_read", help="Only read uart.", default=False)
+@click.option("--port", default="/dev/ttyUSB0", help="Your custom port.")
 @click.option("--baud", default=115200, help="Your custom baudrate.")
-@click.option("--output", default="", help="Your file for logs.")
-def main(port, only_read, baud, output):
-	global running
-	global output_file
-   	
-	if len(output) > 0:
-		try:
-			output_file = open(output, "w", encoding="utf-8")
-		except Exception as e:
-			log_err(f"failed to open output file, error: {e}")
-			return
+@click.option("--output", default=None, help="Your log file")
+@click.option("--not_clear", is_flag=True, help="Dont clear screen.")
+def main(port, baud, output, not_clear):
+    try:
+        monitor = Uartmon(port, baud, output)
+    except serial.SerialException as e:
+        print(f"[error] failed to open serial: {e}")
+        return
 
-	try:
-		ser = create_serial(port, baud)
-	except Exception as e:
-		log_err(f"failed to create serial port, error: {e}.")
-		return
+    monitor.start(not_clear)
 
-	os.system("clear")
-	
-	read_thread = Thread(target=read_data, args=(ser,), daemon=True)
-	
-	if not only_read:
-		write_thread = Thread(target=write_data, args=(ser,), daemon=True)
-		write_thread.start()
-	
-	read_thread.start()
+    try:
+        while True: time.sleep(0.2)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        monitor.stop()
 
-	try:
-		while running:
-			time.sleep(0.2)
-	except KeyboardInterrupt:
-		running = False
-		output_file.close()
-
-if __name__ == "__main__": 
-    main()
+if __name__ == "__main__": main()
